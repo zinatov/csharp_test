1fd074d80d124ae7f3f71aa55048420d3f37dd8b
Ruslan.Zinatov <Ruslan.Zinatov@norbit.ru>
2018-11-10 16:39:01 +0300

    Добавлена проверка путем сравнения списка в тест  EmptyContactCreationTest()

--- addressbook-web-test/addressbook-web-test/tests/ContactCreationTests.cs
+++ addressbook-web-test/addressbook-web-test/tests/ContactCreationTests.cs
@@ -47,7 +47,17 @@ namespace WebAddressbookTests
         public void EmptyContactCreationTest()
         {
             ContactData contact = new ContactData("", "");
+
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+
             app.Contacts.Creation(contact);
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+
+            oldContacts.Add(contact);
+            oldContacts.Sort();
+            newContacts.Sort();
+            Assert.AreEqual(oldContacts, newContacts);
         }
     }
 }
